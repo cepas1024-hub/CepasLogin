aa52b3c88e9cb83fbf925484c60521f521ddb110
cepas1024-hub <cepas1024@gmail.com>
2026-02-18 20:55:37 +0800

    Create CepasLogin.java

---
+++ src/main/java/me/cepas/login/CepasLogin.java
@@ -0,0 +1,11 @@
+package me.cepas.login;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CepasLogin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("CepasLogin enabled!");
+    }
+}
